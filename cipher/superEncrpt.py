d577885f8d25f6bb0050a06d46a9576df59f0d07
willysantoso05 <willysantoso05@gmail.com>
2020-09-07 01:32:29 +0700

    feat: impement superencrypt to GUI

--- cipher/superEncrpt.py
+++ cipher/superEncrpt.py
@@ -9,23 +9,40 @@ from main import Ui_MainWindow
 DEFAULT_NUM_ALPHABET = 26
 
 class SuperEncrypt(Vigenere):
-    # def __init__(self, key: str):
-    #     super().__init__(key)
-    #     super().set_matrix(DEFAULT_NUM_ALPHABET)
+    def __init__(self):
+        super().__init__()
 
     def encrypt(self, plain_text: str, *args, **kwargs) -> str:
-        x = len(plain_text) % self.k_transpose
-        if(x != 0):
-            plain_text += (self.k_transpose - x) * 'z'   #add Z character
-        
-        vigenere_text = super().encrypt(plain_text)
-        return SuperEncrypt.transpose(vigenere_text, self.k_transpose)
+        if(self.validate()):
+            x = len(plain_text) % int(self.k_transpose)
+            if(x != 0):
+                plain_text += (int(self.k_transpose) - x) * 'z'   #add Z character
+
+            vigenere_text = super().encrypt(plain_text)
+            return self.transpose(vigenere_text)
+        return ''
 
 
     def decrypt(self, cipher_text: str, *args, **kwargs) -> str:
-        transpose_text = SuperEncrypt.transpose(cipher_text, self.k_transpose)
-        cipher_text = super().decrypt(transpose_text)
-        return cipher_text
+        if(self.validate()):
+            transpose_text = self.transpose(cipher_text)
+            cipher_text = super().decrypt(transpose_text)
+            return cipher_text
+        return ''
+
+    def validate(self):
+        self.key = Base.remove_punctuation(self.keyText.text())
+        self.k_transpose = Base.remove_punctuation(self.K_Transpose_Input.text(), '[^0-9]')
+
+        if(len(self.key)!=0 and len(self.k_transpose)!=0 and self.k_transpose!='0'):
+            try:
+                k_transpose = int(self.k_transpose)
+                return True
+
+            except:
+                print("INVALID KEY TYPE")
+
+        return False
 
     def render(self, window: Ui_MainWindow):
         self.groupBox_5 = QtWidgets.QGroupBox(window.cipherWidget)
@@ -44,9 +61,9 @@ class SuperEncrypt(Vigenere):
         self.label = QtWidgets.QLabel(self.groupBox_5)
         self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
-        self.lineEdit = QtWidgets.QLineEdit(self.groupBox_5)
-        self.lineEdit.setObjectName("lineEdit")
-        self.horizontalLayout.addWidget(self.lineEdit)
+        self.K_Transpose_Input = QtWidgets.QLineEdit(self.groupBox_5)
+        self.K_Transpose_Input.setObjectName("lineEdit")
+        self.horizontalLayout.addWidget(self.K_Transpose_Input)
         self.verticalLayout_2.addLayout(self.horizontalLayout)
         self.groupBox_4 = QtWidgets.QGroupBox(window.cipherWidget)
         self.groupBox_4.setGeometry(QtCore.QRect(9, 9, 166, 69))
@@ -73,10 +90,8 @@ class SuperEncrypt(Vigenere):
         self.fullModeCheckBox.setText(_translate("MainWindow", "Full Mode"))
         self.label.setText(_translate("MainWindow", "K (Transpose)"))
 
-
-    @staticmethod
-    def transpose(text, k_transpose: int):
-        matrix_char = [list(text[i:i+k_transpose]) for i in range (0, len(text), k_transpose)]
+    def transpose(self, text:str):
+        matrix_char = [list(text[i:i+int(self.k_transpose)]) for i in range (0, len(text), int(self.k_transpose))]
         matrix_transpose = list(itertools.zip_longest(*matrix_char))
         
         result = []
